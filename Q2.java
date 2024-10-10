a9e01ce405a6027ac4922300119678a081feae7b
maryam-9899 <maryamtv954@gmail.com>
2024-10-11 00:23:28 +0300

    Add files via upload

--- Q2.java
+++ Q2.java
@@ -6,44 +6,67 @@ package com.mycompany.q2;
 
 /**
  *
- * مريم العيسي
- * 445005470
+ * 
  */
 public class Q2 {
 
-    class LinearSearch {
+class LinearSearch {
 
-           static int search(int arr[], int n, int x) {
-       
-        for (int i = 0; i < n; i++) {
-           
-            if (arr[i] == x)
-                return i;
-        }
+            // This function returns index of element x in arr[]
 
-        
-        return -1;
-    }
+            static int search(int arr[], int n, int x)
 
-    public static void main(String[] args) {
-        
-        int[] arr = { 3, 4, 1, 7, 5 };
-        int n = arr.length;
+            {
 
-       
-        int x = 1; 
+                        for (int i = 0; i < n; i++) {
 
-        
-        int index = search(arr, n, x);
-        
-        
-        if (index == -1)
-            System.out.println("Element is not present in the array");
-        else
-            System.out.println("Element found at position " + index);
+                                // Return the index of the element if the element
+
+                                    // is found
+
+                                    if (arr[i] == x)
+
+                                                return i;
+
+                        }
+
+ 
+
+                        // return -1 if the element is not found
+
+                        return -1;
+
+            }
+
+ 
+
+            public static void main(String[] args)
+
+            {
+
+                        int[] arr = { 3, 4, 1, 7, 5 };
+
+                        int n = arr.length;
+
+                        int x = 4;
+
+                        int index = search(arr, n, x);
+
+                        if (index == -1)
+
+                          System.out.println("Element is not present in the array");
+
+                        else
+
+                          System.out.println("Element found at position " + index);
 
             }
 
 }
+
+
+    
+            }
+
 
 }
